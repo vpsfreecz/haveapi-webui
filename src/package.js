8ba75e014c44241d161a5857dd28bcbd5c1ecb62
Jakub Skokan <jakub.skokan@havefun.cz>
2016-09-23 22:08:27 +0200

    Add module package: extracts package info from package.json

---
+++ src/package.js
@@ -0,0 +1,5 @@
+var Package = require('../package.json');
+
+export default {
+	version: Package.version,
+};
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
     },
     module: {
         loaders: [
+            { test: /\.json$/, loader: "json-loader" },
             { test: /\.css$/, loader: "style!css" },
             {
 							test: /\.js$/,
